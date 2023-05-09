fd29c712a0d50d61ee9818526bb0b0a4941d9982
fesmjke <fatalerror.sm@gmail.com>
2023-05-09 23:56:20 +0300

    feat: added a world instance, and draw method usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use position::Position;
+use crate::world::World;
+use crate::life::Organism;
+
+mod life;
+mod world;
+mod position;
+
 fn main() {
-    println!("Hello, world!");
+    let mut world = World::new(5,5, vec![Organism::Alive(Position::new(2,2))]);
+
+    world.draw();
 }
