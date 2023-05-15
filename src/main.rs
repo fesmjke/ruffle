f6f4928e9a2abb546fb2653a8a6dceed6a146943
fesmjke <fatalerror.sm@gmail.com>
2023-05-15 23:40:37 +0300

    feat:added a main loop and blinker to world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use position::Position;
+use std::{thread, time, println};
+
 use crate::world::World;
 use crate::life::Organism;
 
@@ -7,7 +8,24 @@ mod world;
 mod position;
 
 fn main() {
-    let mut world = World::new(5,5, vec![Organism::Alive(Position::new(2,2))]);
+    let mut world = World::new(5, 5, vec![Organism::Alive(crate::position::Position { x: 1, y: 1 }),
+            Organism::Alive(crate::position::Position { x: 1, y: 0 }),
+            Organism::Alive(crate::position::Position { x: 1, y: 2 }),
+            ]);
 
-    world.draw();
+    loop {
+        world.draw();
+        world.next();
+
+        thread::sleep(time::Duration::from_millis(1000));
+
+        print!("\x1B[2J\x1B[1;1H");
+
+        if !world.dead() {
+            world.draw();
+            break;
+        }
+    }
+
+    println!("Your world is dead!")
 }
