6ef3ccccc23e1e46532eba9e48730dc4b941239b
fesmjke <fatalerror.sm@gmail.com>
2023-05-15 23:39:35 +0300

    feat: added a impl to Organism enum, and is_alive method

--- src/life.rs
+++ src/life.rs
@@ -5,3 +5,12 @@ pub enum Organism {
     Dead,
     Alive(Position)
 }
+
+impl Organism {
+    pub fn is_alive(&self) -> bool {
+        match self {
+            Organism::Dead => false,
+            Organism::Alive(_) => true,
+        }
+    }
+}
